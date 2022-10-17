b38e227a5e54a328b3dea0e459a25f615ff2bcea
terdegrede <skpr.tffof@gmail.com>
2022-10-17 16:16:11 -0300

    atividade files

--- Atividade7/main.m
+++ Atividade7/main.m
@@ -1,7 +1,7 @@
 clc, clearvars, close all
 % Oscillatory blood flux
 
-dt = 0.01;
+dt = 0.001;
 
 Tu = 0.3;       % Time of systole
 Td = 0.5;       % Time of diastole
@@ -14,14 +14,14 @@ td = Tu+dt : dt : T;
 t = [tu td];
 
 % Signal
-I0 = 10;
+I0 = 450;
 i_tu = I0*sin(pi*tu/Tu);
 i_td = zeros(1, length(td));
 i = [i_tu i_td];
 
 % Periodic signal
 i_t = repmat(i,1 , N);
-t_t = dt: dt :N*(T+dt);
+t_t = dt: dt :N*(T+dt); % Time steps
 
 % Figure
 figure
@@ -33,4 +33,34 @@ axis tight
 axis square
 
 %% Numerical integration
+clc, close all
+R = 1;
+C = 1;
 
+% To solve equation:
+% dp/dt = 1/c(i - P/R)
+
+% Allocation
+p = 80*ones(1, length(t) +1);
+dp = zeros(1, length(t) +1);
+
+% Numerical differentiation
+figure
+for R = 0.5:0.5:2.5
+    for n = 1: length(t_t)
+        dp(n + 1) = 1/C*(i_t(n) - p(n)/ R);
+        p(n+ 1) = dp(n)*dt + p(n);
+        
+    end
+    plot(t_t, p(1:end-1), 'LineWidth', 2)
+    hold on
+end
+
+% plot(t_t, i_t, 'r--'), hold on
+
+%axis([-0.2 max(t_t)+0.2 min(p) max(p) ])
+ axis tight
+legend('R = 0.5', 'R = 1', 'R = 1.5', 'R = 2', 'R = 2.5')
+xlabel('time')
+ylabel('P(t)')
+grid minor
